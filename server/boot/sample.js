aee08772116ac5c6d14186ada539730139401bf9
Jue Hou <juehou@ca.ibm.com>
2016-01-11 18:11:08 -0500

    test include

--- server/boot/sample.js
+++ server/boot/sample.js
@@ -1,24 +1,28 @@
 module.exports = function(app, done) {
   var Space = app.models.space;
   var Customer = app.models.customer;
-  var Account = app.models.account;
   var SpaceCustomer = app.models.spaceCustomer;
+  var City = app.models.city;
 
-  Space.create({name: "space1"}, function(err, space) {
-    Customer.create([{
-      name: "zoe"
-    }, {
-      name: "jelly"
-    }, {
-      name: "janny"
-    }], function(err, customers){
-      customers.forEach(function(customer) {
-        space.customers.add(customer, function(err) {
-          if (err) console.error(err);
+  City.create({name: "city1"}, function(err, city){
+
+    city.spaces.create({name: "space1"}, function(err, space) {
+      Customer.create([{
+        name: "zoe"
+      }, {
+        name: "jelly"
+      }, {
+        name: "janny"
+      }], function(err, customers){
+        customers.forEach(function(customer) {
+          space.customers.add(customer, function(err) {
+            if (err) console.error(err);
+          });
         });
+        done();
       });
-      done();
     });
   });
+
 
 }
