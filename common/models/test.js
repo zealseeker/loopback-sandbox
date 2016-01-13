ce918e442fc3bb2d31b2506d530c620893fb399f
Jue Hou <juehou@ca.ibm.com>
2016-01-13 11:49:28 -0500

    Fix param name

--- common/models/test.js
+++ common/models/test.js
@@ -3,12 +3,12 @@ module.exports = function(Test) {
     // Here is another way to register hook for data updating
     // Please see doc of operation hook for more details
     // https://docs.strongloop.com/display/public/LB/Operation+hooks
-    
+
 	    // Test.observe('after save',function(ctx, next){
 	    //     console.log('upsert called: ', ctx.isNewInstance);
 	    //     next();
 	    // });
-    Test.beforeRemote('upsert',function(ctx, user, next){
+    Test.beforeRemote('upsert',function(ctx, modelInstance, next){
         console.log('upsert called');
         next();
     });
