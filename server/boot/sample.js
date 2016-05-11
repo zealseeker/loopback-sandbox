131a78113a1771ad14fbf35461e0d44ac5033096
zealseeker <yanyanghong@163.com>
2016-05-11 20:12:15 +0800

    add model to find the bug of #1976

--- server/boot/sample.js
+++ server/boot/sample.js
@@ -5,7 +5,16 @@ module.exports = function(app, done) {
     "name3": "thirdname"
   }, function(err, test){
     console.log(test, " created");
-    done();
+   
+  });
+  var Auser = app.models.auser;
+  Auser.create({
+	"username":"123",
+	"password":"123",
+	"email":"123@123.com"
+  },function(err,auser){
+	console.log(auser," created");
+	done();
   });
   
 }
